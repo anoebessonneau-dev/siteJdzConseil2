c0099a7742eee18e5869ba7dec2730d47419eab1
Anoé Bessonneau <anoe.bessonneau@gmail.com>
2026-01-22 00:51:20 +0100

    Maj d'après soutenance

---
+++ js/site.js
@@ -0,0 +1,13 @@
+function afficherContact() {
+    // Pemret que les données ne figurents pas dans le code html et qu'elles soient cachées
+    const email = ["j","d","z","c","o","n","s","e","i","l","@","g","m","a","i","l",".","f","r"].join("");
+    const tel = ["0","6",".","1","2",".","3","4",".","5","6",".","7","8"].join("");
+    const tel2 = ["0","2",".","4","1",".","3","0",".","3","5",".","2","4"].join("");
+
+    document.getElementById("contactInfo").innerHTML =
+        email + "<br>" + tel + "<br>" + tel2;
+}
+
+function menuBurger() {
+
+}
